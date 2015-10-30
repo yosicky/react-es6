e3ca172903ed9beb423b2b2c6585a30fa39868de
yamamoto-yoshi <yamamoto-yoshi@dmm.com>
2015-10-30 18:56:55 +0900

    first commit

---
+++ app/js/app.js
@@ -0,0 +1,63 @@
+/**
+ * test
+ */
+
+
+class Hello extends React.Component {
+
+    // es6では必要 内容がsuper(props)だけのときは省略可能
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="container" id="hoo">
+                Hello {this.props.name}
+                {/*<Counter />*/}
+            </div>
+        );
+    }
+
+}
+
+class Counter extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            count: 0
+        };
+    }
+/*
+    getInitialState() {
+        return {
+            count: 0
+        };
+    }
+*/
+    onClick() {
+        this.setState({count: this.state.count + 1});
+    }
+
+    render() {
+        return (
+            <div>
+                <div>count:{this.state.count}</div>
+                <button onClick={this.onClick.bind(this)}>click!</button>
+            </div>
+        );
+    }
+}
+
+
+ReactDOM.render(
+    <Hello name="React" />,
+    document.getElementById('content')
+);
+
+ReactDOM.render(
+    <Counter />,
+    document.getElementById('content2')
+);
+
